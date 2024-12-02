76c13c16ca2b6eb3a27f78fa2888035af9d9b746
robda20188 <danielrouco188rr@gmail.com>
2024-12-02 19:34:26 +0100

    Moving some code to a new function (safe())

--- day2/part1.c
+++ day2/part1.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 
+int safe(int report[]){
+    int safe = 1;
+    int abs_distance;
+    int real_distance;
+    int ascendent = -1;
+    int level_n = 0;
+    while(report[level_n + 1] != -1 && safe){
+        abs_distance = report[level_n] - report[level_n + 1];
+        real_distance = abs_distance;
+        if(abs_distance < 0){
+            abs_distance *= -1;
+        }
+
+        if(abs_distance < 1 || abs_distance > 3){
+            safe = 0;
+        }else if(ascendent == -1){
+            if(real_distance < 0){
+                ascendent = 1;
+            }else{
+                ascendent = 0;
+            } 
+        }else if(ascendent && real_distance > 0){
+            safe = 0;
+        }else if(!ascendent && real_distance < 0){
+            safe = 0;
+        }
+        level_n++;
+    }
+    return safe;
+}
+
 int main(){
     //Input
     int reports[1000][20];
@@ -21,41 +52,9 @@ int main(){
     int reports_length = report_n + 1;
     
     report_n = 0;
-    level_n = 0;
-    int safe;
     int safe_reports = 0;
-    int abs_distance;
-    int real_distance;
-    int ascendent;
     for(; report_n < reports_length; report_n++){
-        level_n = 0;
-        safe = 1;
-        ascendent = -1;
-        while(reports[report_n][level_n + 1] != -1 && safe){
-            abs_distance = reports[report_n][level_n] - reports[report_n][level_n + 1];
-            real_distance = abs_distance;
-            if(abs_distance < 0){
-                abs_distance *= -1;
-            }
-
-            if(abs_distance < 1 || abs_distance > 3){
-                safe = 0;
-            }else if(ascendent == -1){
-                if(real_distance < 0){
-                    ascendent = 1;
-                }else{
-                    ascendent = 0;
-                } 
-            }else if(ascendent && real_distance > 0){
-               safe = 0;
-            }else if(!ascendent && real_distance < 0){
-                safe = 0;
-            }
-            level_n++;
-        }
-        if(safe){
-            safe_reports++;
-        }
+        safe_reports += safe(&reports[report_n][0]);
     }
     printf("There are %d safe reports\n", safe_reports);
 }
