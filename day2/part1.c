43684586fe794b057f48fd1b91109151d7d48074
robda20188 <danielrouco188rr@gmail.com>
2024-12-02 18:43:40 +0100

    Refactoring code

--- day2/part1.c
+++ day2/part1.c
@@ -24,38 +24,32 @@ int main(){
     level_n = 0;
     int safe;
     int safe_reports = 0;
-    int distance;
+    int abs_distance;
+    int real_distance;
     int ascendent;
     for(; report_n < reports_length; report_n++){
         level_n = 0;
         safe = 1;
         ascendent = -1;
-        while(reports[report_n][level_n] != -1 && safe){
-            if(reports[report_n][level_n + 1] != -1){
-                distance = reports[report_n][level_n] - reports[report_n][level_n + 1];
-                if(distance < 0){
-                    distance *= -1;
-                }
-                if(distance < 1 || distance > 3){
-                    printf("Incorrect distance between %d and %d in line %d\n", reports[report_n][level_n], reports[report_n][level_n + 1], report_n + 1);
-                    safe = 0;
-                    continue;
-                }
-                if(ascendent == -1){
-                    if(reports[report_n][level_n] - reports[report_n][level_n + 1] < 0){
-                        ascendent = 1;
-                    }else{
-                        ascendent = 0;
-                    } 
-                }else if(ascendent && reports[report_n][level_n] - reports[report_n][level_n + 1] > 0){
-                    printf("Expected greater than %d but found %d in line %d\n", reports[report_n][level_n], reports[report_n][level_n + 1], report_n + 1);
-                   safe = 0;
-                   continue; 
-                }else if(!ascendent && reports[report_n][level_n] - reports[report_n][level_n + 1] < 0){
-                    printf("Expected less than %d but found %d in line %d\n", reports[report_n][level_n], reports[report_n][level_n + 1], report_n + 1);
-                    safe = 0;
-                    continue;
-                }
+        while(reports[report_n][level_n + 1] != -1 && safe){
+            abs_distance = reports[report_n][level_n] - reports[report_n][level_n + 1];
+            real_distance = abs_distance;
+            if(abs_distance < 0){
+                abs_distance *= -1;
+            }
+
+            if(abs_distance < 1 || abs_distance > 3){
+                safe = 0;
+            }else if(ascendent == -1){
+                if(real_distance < 0){
+                    ascendent = 1;
+                }else{
+                    ascendent = 0;
+                } 
+            }else if(ascendent && real_distance > 0){
+               safe = 0;
+            }else if(!ascendent && real_distance < 0){
+                safe = 0;
             }
             level_n++;
         }
@@ -63,5 +57,5 @@ int main(){
             safe_reports++;
         }
     }
-    printf("\nThere are %d safe reports\n", safe_reports);
+    printf("There are %d safe reports\n", safe_reports);
 }
